4c37e53f90914663e74c570bdafc984cd24f3650
gyenios <98366207+gyenios@users.noreply.github.com>
2024-05-15 04:03:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -21,3 +21,35 @@ def new_user():
                                 break    
             except Exception as e:
                 print('An error occured: ', e)
+
+def login():
+
+    # This function checks if the entered profile has a match in the 'profiles.csv' file 
+    def authenticate(user,key):
+        with open('profiles.csv', 'r') as file:
+            reader = csv.dictReader(file)
+            for row in reader:
+                if row['User'] == user:
+                    if row['Key'] == key:
+                        checkpoint = int(row['Checkpoint'])
+                        print('--- Login successful ---')
+                        return [True,checkpoint]
+                else:
+                    return [False,'X']
+    # Profile details entry and authentication       
+    tries = 0
+    while tries < 3:
+
+        # Taking profile details (username and key)
+        user = input('Username: ')
+        key = input('Enter your 4-digit key: ')
+        
+        # Profile authentication
+        logged,checkpoint = authenticate(user,key)
+        if logged == True:
+            print(f'Welcome back {user}')
+            break
+        else:
+            print('Wrong username or password')
+            tries += 1
+    print('Too many wrong entries')
